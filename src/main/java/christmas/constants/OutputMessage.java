a3fe513d4242ba875bd5cba9e3576c7b7a42765b
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2023-11-10 18:00:08 +0900

    refactor(OutputMessage): Class 에서 Enum으로 변환 및 변화에 의한 수정

--- src/main/java/christmas/constants/OutputMessage.java
+++ src/main/java/christmas/constants/OutputMessage.java
@@ -1,9 +1,8 @@
-package christmas.view;
+package christmas.constants;
 
 public enum OutputMessage {
 
-    REQUEST_DATE_MESSAGE("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)"),
-    ;
+    REQUEST_DATE_MESSAGE("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
 
 
     private final String message;
