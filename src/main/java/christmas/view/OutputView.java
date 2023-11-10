cdaa7b5740b26a6bc7218d5d399c8435f02775f5
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2023-11-10 18:00:08 +0900

    refactor(OutputMessage): Class 에서 Enum으로 변환 및 변화에 의한 수정

---
+++ src/main/java/christmas/view/OutputView.java
@@ -0,0 +1,17 @@
+package christmas.view;
+
+public class OutputView {
+    public void printErrorMessage(String message) {
+        System.out.println(message);
+    }
+
+    public void printRequestDate(OutputMessage message) {
+        System.out.println(message.getMessage());
+    }
+
+    public void printMenu() {
+        System.out.println("<주문 메뉴>");
+        // ...
+    }
+    // ...
+}
