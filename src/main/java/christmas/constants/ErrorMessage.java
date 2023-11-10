9be5ac7c192e6c83ea53f705d154824be1b0c637
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2023-11-10 17:58:20 +0900

    refactor(ErrorMessage): class에서 enum으로 변환

--- src/main/java/christmas/constants/ErrorMessage.java
+++ src/main/java/christmas/constants/ErrorMessage.java
@@ -1,6 +1,16 @@
 package christmas.constants;
 
-public class ErrorMessage {
-    public static final String INVALID_DATE_MESSAGE = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
-    public static final String INVALID_ORDER_MESSAGE = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
+public enum ErrorMessage {
+    INVALID_DATE_MESSAGE("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요."),
+    INVALID_ORDER_MESSAGE("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
