5b7624922c983b70a4e47fad0e27990fc5e773cf
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2023-11-11 19:52:22 +0900

    style: 변수명 변경 및 변경에 의한 수정

--- src/main/java/christmas/exception/InvalidDateException.java
+++ src/main/java/christmas/exception/InvalidDateException.java
@@ -4,7 +4,7 @@ import christmas.constants.ErrorMessage;
 
 public class InvalidDateException extends IllegalArgumentException {
     //발생시마다 호출하는 것이 아닌 한번 띄어 놓고 가져오면서 사용하기 위함(new 생성을 막아주기 위해서)
-    public static final InvalidDateException invalidDateException = new InvalidDateException();
+    public static final InvalidDateException exception = new InvalidDateException();
 
     private InvalidDateException() {
         super(ErrorMessage.INVALID_DATE_MESSAGE.getMessage());
