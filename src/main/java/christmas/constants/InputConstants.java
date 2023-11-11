845c9d58fa7f1af8367f176324f6b50428008915
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2023-11-11 19:56:05 +0900

    feat: 사용자가 주문할 메뉴와 개수를 입력할때 입력에서의 검증 구현
    - , (comma)를 기준으로 자른 문자열이 비어있을때 예외 발생
    - , (comma)로 나눠진 값이 '-' 표시 1개를 갖지 않는다면 예외 발생
    - ' - ' 표시로 나눠진 값이 (음식-개수) 형태를 갖지 않는다면 예외 발생
    - 개수가 개수가 숫자가 아니라면 예외 발생
    - 개수가 Int 형의 최대값을 넘길경우 예외 발생

---
+++ src/main/java/christmas/constants/InputConstants.java
@@ -0,0 +1,11 @@
+package christmas.constants;
+
+public class InputConstants {
+    public static final String SPLIT_SIGN = ",";
+    public static final char SPLIT_SIGN_TO_CHAR = ',';
+    public static final String DISTINGUISH_SIGN = "-";
+    public static final char DISTINGUISH_SIGN_TO_CHAR = '-';
+    public static final String NUMBER_REGEX = "[0-9]+";
+    public static final String EMPTY_INPUT = "";
+    public static final int ONE_COUNT = 1;
+}
--- src/main/java/christmas/validator/InputValidator.java
+++ src/main/java/christmas/validator/InputValidator.java
@@ -1,9 +1,18 @@
 package christmas.validator;
 
+import static christmas.constants.InputConstants.DISTINGUISH_SIGN_TO_CHAR;
+import static christmas.constants.InputConstants.EMPTY_INPUT;
+import static christmas.constants.InputConstants.NUMBER_REGEX;
+import static christmas.constants.InputConstants.ONE_COUNT;
+import static christmas.constants.InputConstants.SPLIT_SIGN;
+import static christmas.constants.InputConstants.SPLIT_SIGN_TO_CHAR;
+
 import christmas.exception.InvalidDateException;
+import christmas.exception.InvalidOrderException;
+import java.util.Arrays;
+import java.util.List;
 
 public class InputValidator {
-    private static final String NUMBER_REGEX = "[0-9]+";
 
 
     public static void notCorrectNumber(String input) {
@@ -15,20 +24,43 @@ public class InputValidator {
         try {
             Integer.parseInt(input);
         } catch (NumberFormatException e) {
-            throw InvalidDateException.invalidDateException;
+            throw InvalidDateException.exception;
         }
     }
 
     private static void notNumber(String input) {
         if (isNotMatchRegex(input)) {
-            throw InvalidDateException.invalidDateException;
+            throw InvalidDateException.exception;
         }
     }
 
     private static boolean isNotMatchRegex(String input) {
-        if (input.matches(InputValidator.NUMBER_REGEX)) {
+        if (input.matches(NUMBER_REGEX)) {
             return false;
         }
         return true;
+    }
+
+    public static void splitInputEmpty(String input) {
+        List<String> splitInput = Arrays.stream(input.split(SPLIT_SIGN)).toList();
+        if (splitInput.contains(EMPTY_INPUT)) {
+            throw InvalidOrderException.exception;
+        }
+    }
+
+    public static void notOneCountDistinguishSign(String input) {
+        int signCount = (int) input.chars()
+                .filter((c) -> c == DISTINGUISH_SIGN_TO_CHAR)
+                .count();
+        if (signCount != (ONE_COUNT)) {
+            throw InvalidOrderException.exception;
+        }
+    }
+
+    public static void endOfInputNotSplitSign(String input) {
+        char endOfInput = input.charAt(input.length() - 1);
+        if (endOfInput == SPLIT_SIGN_TO_CHAR) {
+            throw InvalidOrderException.exception;
+        }
     }
 }
