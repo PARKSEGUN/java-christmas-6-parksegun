6e5ca0ec38a7fe6f669f87aafadfe5075b7b2575
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2023-11-14 01:35:19 +0900

    refactor(model): 메뉴와 개수를 입력할때 예외처리 변경에 대한 수정
    이전의 예외 조건 : 나눠진 값이 - 표시 한개를 포함하지 않을때
    새로운 예외 조건 : (comma)로 나눠진 값이 정규식을 따르지 않는다면 예외 발생

--- src/main/java/christmas/constants/InputConstants.java
+++ src/main/java/christmas/constants/InputConstants.java
@@ -8,4 +8,7 @@ public final class InputConstants {
     public static final String NUMBER_REGEX = "[0-9]+";
     public static final String EMPTY_INPUT = "";
     public static final int ONE_COUNT = 1;
+
+
+    public static final String INPUT_ORDER_FORMAT = "^[가-힣]+-[0-9]+";
 }
--- src/test/java/christmas/validator/inputValidatorTest.java
+++ src/test/java/christmas/validator/inputValidatorTest.java
@@ -75,48 +75,48 @@ public class inputValidatorTest {
         );
     }
 
+//    @ParameterizedTest
+//    @DisplayName("(comma)를 기준으로 자른 문자열이 비어있을때 예외 발생")
+//    @MethodSource
+//    void splitInputIsEmpty(String input) {
+//        //given
+//        //when
+//        Throwable result = catchThrowable(() -> InputValidator.splitInputEmpty(input));
+//        //then
+//        assertThat(result).isInstanceOf(IllegalArgumentException.class);
+//    }
+
+//    private static Stream<Arguments> splitInputIsEmpty() {
+//        return Stream.of(
+//                Arguments.of("1,,2"),
+//                Arguments.of("1,2,3,,4"),
+//                Arguments.of("1,2,,3"),
+//                Arguments.of("1,,,,,5")
+//        );
+//    }
+//
+//    @ParameterizedTest
+//    @DisplayName("(comma)를 기준으로 자른 문자열이 비어있지 않다면 정상")
+//    @MethodSource
+//    void splitInputIsNotEmpty(String input) {
+//        //given
+//        //when
+//        Throwable result = catchThrowable(() -> InputValidator.splitInputEmpty(input));
+//        //then
+//        assertThat(result).doesNotThrowAnyException();
+//    }
+//
+//    private static Stream<Arguments> splitInputIsNotEmpty() {
+//        return Stream.of(
+//                Arguments.of("1,2,3"),
+//                Arguments.of("1,2,3,4,5"),
+//                Arguments.of("1,2,3,4"),
+//                Arguments.of("1,2,3,4,5")
+//        );
+//    }
+
     @ParameterizedTest
-    @DisplayName("(comma)를 기준으로 자른 문자열이 비어있을때 예외 발생")
-    @MethodSource
-    void splitInputIsEmpty(String input) {
-        //given
-        //when
-        Throwable result = catchThrowable(() -> InputValidator.splitInputEmpty(input));
-        //then
-        assertThat(result).isInstanceOf(IllegalArgumentException.class);
-    }
-
-    private static Stream<Arguments> splitInputIsEmpty() {
-        return Stream.of(
-                Arguments.of("1,,2"),
-                Arguments.of("1,2,3,,4"),
-                Arguments.of("1,2,,3"),
-                Arguments.of("1,,,,,5")
-        );
-    }
-
-    @ParameterizedTest
-    @DisplayName("(comma)를 기준으로 자른 문자열이 비어있지 않다면 정상")
-    @MethodSource
-    void splitInputIsNotEmpty(String input) {
-        //given
-        //when
-        Throwable result = catchThrowable(() -> InputValidator.splitInputEmpty(input));
-        //then
-        assertThat(result).doesNotThrowAnyException();
-    }
-
-    private static Stream<Arguments> splitInputIsNotEmpty() {
-        return Stream.of(
-                Arguments.of("1,2,3"),
-                Arguments.of("1,2,3,4,5"),
-                Arguments.of("1,2,3,4"),
-                Arguments.of("1,2,3,4,5")
-        );
-    }
-
-    @ParameterizedTest
-    @DisplayName("(comma)로 나눠진 값이 '-' 표시 1개를 갖지 않는다면 예외 발생")
+    @DisplayName("(comma)로 나눠진 값이 정규식을 따르지 않는다면 예외 발생")
     @MethodSource
     void distinguishSignCountIsNotOne(String input) {
         //given
@@ -128,15 +128,15 @@ public class inputValidatorTest {
 
     private static Stream<Arguments> distinguishSignCountIsNotOne() {
         return Stream.of(
-                Arguments.of("--"),
-                Arguments.of("1-1-"),
-                Arguments.of("asdf--5"),
-                Arguments.of("파스타--5")
+                Arguments.of("가--3"),
+                Arguments.of("-가-3"),
+                Arguments.of("5-5"),
+                Arguments.of("A-5")
         );
     }
 
     @ParameterizedTest
-    @DisplayName("(comma)로 나눠진 값이 '-' 표시 1개를 갖는 다면 정상")
+    @DisplayName("(comma)로 나눠진 값이 정규식을 따른다면 정상")
     @MethodSource
     void distinguishSignCountIsOne(String input) {
         //given
@@ -148,12 +148,10 @@ public class inputValidatorTest {
 
     private static Stream<Arguments> distinguishSignCountIsOne() {
         return Stream.of(
-                Arguments.of("-"),
-                Arguments.of("1-1"),
-                Arguments.of("asdf-5"),
+                Arguments.of("가-1"),
+                Arguments.of("나-1"),
+                Arguments.of("다-5"),
                 Arguments.of("파스타-5")
         );
     }
-
-
 }
