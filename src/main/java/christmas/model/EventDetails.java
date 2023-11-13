47885b66b996fdd185d64b9a3c05c59de410a2a7
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2023-11-13 22:51:10 +0900

    refactor(model): 메서드 분할

--- src/main/java/christmas/model/EventDetails.java
+++ src/main/java/christmas/model/EventDetails.java
@@ -1,5 +1,6 @@
 package christmas.model;
 
+import static christmas.constants.OutputMessage.EVENT_DETAIL_FORMAT;
 import static christmas.constants.OutputMessage.GIFT_INFO_FORMAT;
 import static christmas.constants.OutputMessage.NO_GIFT_FORMAT;
 import static christmas.constants.model.EventConstants.NO_DISCOUNT;
@@ -9,6 +10,10 @@ import static christmas.constants.model.Menu.CHAMPAGNE;
 import christmas.constants.model.EventName;
 import java.text.DecimalFormat;
 import java.util.Map;
+
+/*
+ *  이벤트의 할인금액에 대한 정보를 담당
+ * */
 
 public class EventDetails {
 
@@ -22,32 +27,38 @@ public class EventDetails {
         return new EventDetails(details);
     }
 
-    public int findAllDiscount() {
+    public int sumOfDiscounts() {
         return details.values().stream()
                 .mapToInt(i -> i)
                 .sum();
     }
 
-    public String giftInfoToString() {
+    public String toStringGiftEvent() {
         if (details.get(GIFT_EVENT) == CHAMPAGNE.getPrice()) {
             return String.format(GIFT_INFO_FORMAT.getMessage(), CHAMPAGNE.getName());
         }
         return NO_GIFT_FORMAT.getMessage();
     }
 
-    public String discountInfoToString() {
-        if (findAllDiscount() == NO_DISCOUNT) {
-            return "없음";
-        }
+    public String toStringDiscountInfo() {
         DecimalFormat decimalFormat = new DecimalFormat();
         StringBuilder discountInfo = new StringBuilder();
+        if (sumOfDiscounts() == NO_DISCOUNT) {
+            return "없음";
+        }
         for (EventName eventName : details.keySet()) {
-            Integer discount = details.get(eventName);
-            if (discount != NO_DISCOUNT) {
-                discountInfo.append(eventName.getEventName()).append(": -").append(decimalFormat.format(discount))
-                        .append("원\n");
-            }
+            discountInfo.append(makeDiscountInfo(decimalFormat, eventName));
         }
         return discountInfo.toString();
+    }
+
+    private String makeDiscountInfo(DecimalFormat decimalFormat, EventName eventName) {
+        Integer discount = details.get(eventName);
+        if (discount > NO_DISCOUNT) {
+            return String.format(
+                    EVENT_DETAIL_FORMAT.getMessage(), eventName.getName(), decimalFormat.format(discount)
+            );
+        }
+        return "";
     }
 }
